a86f70f0a2639b855e297fcf7f8d557fb52003de
Akvedo <ani.pimpale@gmail.com>
2018-01-13 04:32:38 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var dbUrl = 'mongodb://user:user@ds053774.mlab.com:53774/todolist'; 
+
+var Task = mongoose.model('Task', {
+    task: String,
+    status: String
+});
+//var task = ["anirocks", "sex"];
+//var task = [{ "task": "fuck", "status": "no" }, {"task": "suck", "status": "yes"}]
+var task =[];
+
+app.get('/tasks', (req,res) => {
+    Task.find({}, (err, tasks) => {
+        res.send(tasks);
+    });
+});
+
+app.post('/tasks', (req,res) => {
+    if(req.body.status == "yes") {
+        var todo = new Task(req.body);
+        console.log(todo);
+        
+        Task.findOneAndUpdate( {task: req.body.task, status: "no"}, {$set:{status:"yes"}}, (err) => {
+            if(err) {
+                console.log(err);
+            }
+        });
+
+        io.emit('taskStatus', req.body);
+        res.sendStatus(200);
+    }
+    else {
+        var todo = new Task(req.body);
+        console.log(todo);
+        todo.save((err) => {
+            if (err) {
+                sendStatus(500);
+            }
+
+            io.emit('taskAdd', req.body);
+            res.sendStatus(200);
+        });
+    }
+
+        
+});
+
+io.on('connection', (socket) => {
+    console.log("Someone joined");
+});
+
+mongoose.connect(dbUrl, {useMongoClient: true} , (err) => {
+    console.log("mongo connected", err);
+});
+
+var server = http.listen(8080, () => {
+    console.log("Server is listen on port", server.address().port);
+});
